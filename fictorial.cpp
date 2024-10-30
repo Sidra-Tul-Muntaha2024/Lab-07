e2884edc41b475890e528e1cd9bbfa6a51faa6e1
Sidra-Tul-Muntaha2024 <sidra.muntaha@riphah.edu.pk>
2024-10-30 09:36:59 +0500

    Add files via upload

---
+++ fictorial.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//	 5! 5x4x3x2x1=12086766969	
+    int n;
+    long factorial = 1.0;
+  cout << "Enter a positive integer: ";
+   cin >> n;
+
+   if (n < 0)	 
+	 cout << "Error! Factorial of a negative number doesn't exist.";
+   
+
+ else {
+        for(int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;    
+   }
+
+    return 0;
+}
+
